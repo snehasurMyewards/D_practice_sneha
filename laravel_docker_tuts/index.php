c555e165babdbc16a5b78566f69931f05a257ab0
codecaptain <snehasur.myewards@gmail.com>
2024-10-21 15:14:22 +0530

    first commit

---
+++ laravel_docker_tuts/index.php
@@ -0,0 +1,22 @@
+<?php
+phpinfo();
+?>
+
+<!-- insall docker
+	docker-compose build --no-cache --force-rm
+   docker-compose up -d
+   docker exec -it laravel_docker_tuts bash
+   composer create-project laravel/laravel:^11.0 doc_app
+
+cd doc_app
+root@fe95f920c720:/var/www/html/doc_app# ls
+README.md  artisan    composer.json  config    package.json  postcss.config.js  resources  storage             tests   vite.config.js
+app        bootstrap  composer.lock  database  phpunit.xml   public             routes     tailwind.config.js  vendor
+root@fe95f920c720:/var/www/html/doc_app# chmod -R 777
+chmod: missing operand after '777'
+Try 'chmod --help' for more information.
+root@fe95f920c720:/var/www/html/doc_app# chmod -R 777 storage
+root@fe95f920c720:/var/www/html/doc_app# chmod -R 777 storage/logs
+root@fe95f920c720:/var/www/html/doc_app# chmod -R 777 bootstrap/cache
+root@fe95f920c720:/var/www/html/doc_app#
+ -->
